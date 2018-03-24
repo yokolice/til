aeb78172ea72a5b6c2b76c5904933638a5a3d33d
EC2 Default User <ec2-user@ip-172-31-24-195.us-west-2.compute.internal>
2018-03-24 12:56:29 +0000

    演習1

---
+++ enshu1.rb
@@ -0,0 +1,14 @@
+my_library = Library.new
+Book.new(:author => "Herman Melville", :title => "Moby-Dick", :library => my_library)
+Book.new(:author => "Hans Christian Andersen", :title => "The Ugly Duckling", :library => my_library)
+puts my_library
+
+class Library
+  def initialize(my_library)
+    @my_library = my_library
+  end
+
+class Book
+  def initialize{Title: Author:}
+    @{Title: Author:} = {Title: Author:}
+  end
